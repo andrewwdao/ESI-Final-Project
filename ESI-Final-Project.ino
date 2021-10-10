1dd7c8deaa3a35b9d32975268c6d48a3de3bbad4
Minh-An DAO <29350324+minhan74@users.noreply.github.com>
2021-10-10 09:07:51 +0700

    update system mode through rfid

--- ESI-Final-Project.ino
+++ ESI-Final-Project.ino
@@ -36,30 +36,24 @@
 #define CARDS_KNOWN     2
 #define CARD_SIZE       4
 // card data
-byte knownCards[CARDS_KNOWN][CARD_SIZE] = {
-  {0x93, 0x47, 0xe0, 0x00},
-  {0xe5, 0xa2, 0x9c, 0x2c}
+const byte RFID_KEY[CARDS_KNOWN][CARD_SIZE] = {
+  {0xFA, 0x20, 0x7B, 0x2E},
+  {0xe5, 0xa2, 0x9c, 0x2F}
 };
-
-char name[CARDS_KNOWN][10] = {
-  "James",
-  "Richard"
-};
-
-char password[CARDS_KNOWN][5] = {
-  "1234", 
-  "8888"
-};
-
 
 // --- servo angles
-#define ON_DEFAULT          0
 #define ON_TRIGGER          90
 #define ON_RELEASE          0
-#define OFF_DEFAULT         0
-#define OFF_TRIGGER         180
+#define ON_DEFAULT          ON_RELEASE
+#define OFF_TRIGGER         90
 #define OFF_RELEASE         0
+#define OFF_DEFAULT         OFF_TRIGGER
 
+enum {
+  LDR,
+  SOUND,
+  PIR
+} system_state;
 
 uint8_t sound_val = 0;
 uint16_t ldr_val = 0;
@@ -67,9 +61,10 @@ uint8_t  pir_val = 0;
 uint8_t  id_buffer[CARD_SIZE];  // Matrix for storing new UID
 
 MFRC522 rfid(RFID_SS_PIN, RFID_RST_PIN);  // Create MFRC522 instance
-Servo on_servo, off_servo;                // 2 servo instances
+Servo on_servo, off_servo;                // Create 2 servo instances
+Timer<1, millis> timer; // create a timer with 1 task and millisecond resolution
 
-Timer<1, millis> timer; // create a timer with 1 task and microsecond resolution
+// --- Buzzer handler
 volatile uint8_t BUZZER_FLAG = 0;
 bool toggle_buz(void *) {
   if (BUZZER_FLAG)
@@ -79,9 +74,13 @@ bool toggle_buz(void *) {
   }
   return true;
 }
+// ---
 
 void setup () 
 {
+  // --- System state initialize
+  system_state = LDR;
+
   // --- Sensors initialize
   pinMode(SOUND_SENSOR_PIN,INPUT);
   pinMode(LDR_PIN,INPUT);
@@ -96,12 +95,13 @@ void setup ()
   // --- servo initialize
   on_servo.attach(SERVO_ONSWITCH);
   off_servo.attach(SERVO_OFFSWITCH);
-  on_servo.write(0); off_servo.write(0);
+  on_servo.write(ON_DEFAULT); off_servo.write(OFF_DEFAULT);
   
   // --- buzzer initialize
   pinMode(BUZZER_PIN,OUTPUT);
   timer.every(100, toggle_buz); //100ms
-  //
+  
+  // --- Serial initialize
   Serial.begin (9600);
   Serial.println("System ready!");
 }
@@ -109,22 +109,22 @@ void setup ()
 void loop () 
 {
   timer.tick(); // tick the timer
-  
-  // ----------------- LDR data collection -----------
-  ldr_val = analogRead(LDR_PIN);
-  Serial.print("LDR:"); Serial.println(ldr_val, DEC);
-  // ----------------- Sound data collection -----------
-  sound_val = digitalRead(SOUND_SENSOR_PIN);
-  // Serial.print("Sound:");
-  Serial.println(sound_val, DEC);
-  // ----------------- PIR data collection -----------
-  pir_val = digitalRead(PIR_PIN);
-  Serial.print("PIR:"); Serial.println(pir_val, DEC);
   // ----------------- RFID data collection -----------
   if (getRFID()) {
-    Serial.print("RFID:"); dump_byte_array(id_buffer,CARD_SIZE);
-    Serial.println();
+    Serial.print("RFID:"); dump_byte_array(id_buffer,CARD_SIZE); Serial.println();
+    checkUID(); // mode changing integrated inside
   }
+  // // ----------------- LDR data collection -----------
+  // ldr_val = analogRead(LDR_PIN);
+  // Serial.print("LDR:"); Serial.println(ldr_val, DEC);
+  // // ----------------- Sound data collection -----------
+  // sound_val = digitalRead(SOUND_SENSOR_PIN);
+  // // Serial.print("Sound:");
+  // Serial.println(sound_val, DEC);
+  // // ----------------- PIR data collection -----------
+  // pir_val = digitalRead(PIR_PIN);
+  // Serial.print("PIR:"); Serial.println(pir_val, DEC);
+  
   // ----------------- Servo control -----------
   on_servo.write(ON_TRIGGER); off_servo.write(OFF_TRIGGER);
 
@@ -133,7 +133,7 @@ void loop ()
 }
 
 // #################### Additional RFID helper functions ############################
-bool getRFID() {
+bool getRFID(void) {
   bool isPICCpresent = false;
   if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial())
   { //if new card is found
@@ -144,6 +144,41 @@ bool getRFID() {
   rfid.PCD_StopCrypto1(); // Stop encryption on PCD
   return isPICCpresent;          // returns TRUE if PICC is detected, false otherwise
 }//end getRFID
+
+/*Routine for checking if the UID is correct*/
+bool checkUID(void){
+  for (byte i=0; i<CARDS_KNOWN; i++)
+  {
+    //counter for correct UID parts, there are CARD_SIZE parts in an UID, if 1 part correct, this counter will increase.
+    //If all of them are correct (the UID is granted) then this counter will equal CARD_SIZE
+    byte Rcounter=0; 
+    for (byte j = 0; j < CARD_SIZE; j++){
+      if (RFID_KEY[i][j]==id_buffer[j]) {Rcounter++;}
+    }//end for
+    if (Rcounter==4) { //access granted
+      Serial.print(F("Mode updated to: "));
+      system_state_update();
+      return true;
+    }//end if
+  }
+  return false;
+}//end checkUID
+
+void system_state_update() {
+  switch (system_state)
+  {
+  case LDR:
+    system_state = SOUND; Serial.println("SOUND mode");
+    break;
+  case SOUND:
+    system_state = PIR; Serial.println("PIR mode");
+    break;
+  case PIR:
+    system_state = LDR; Serial.println("LDR mode");
+  default:
+    break;
+  }
+}
 
 /**
  * Helper routine to dump a byte array as hex values to Serial.
